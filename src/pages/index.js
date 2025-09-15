773bf9160f08c24e28c672302629e05c223faa4b
boris.ayupov <boris.ayupov@gmail.com>
2025-09-15 16:23:14 -0400

    Updates

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as MainPage } from "./MainPage"
-export { default as HomeStaging } from "./HomeStaging"
-export { default as FurnishAndStyle } from "./FurnishAndStyle"
-export { default as VacationRental } from "./VacationRental"
-export { default as Portfolio } from "./Portfolio"
-export { default as GetAQuote } from "./GetAQuote"
-export { default as ContactUs } from './ContactUs';
